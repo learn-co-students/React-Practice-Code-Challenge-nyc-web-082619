df4e55f684b2be33a1af57970e0599b91696ba8e
github username <github email address>
2019-11-05 11:25:58 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,47 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state={
+    sushis: [],
+    sushisEaten: [],
+    money: 100
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(sushis=> this.setState({
+      sushis: sushis
+    }))
+  }
+
+  eatSushi = id => {
+    const sushi = this.state.sushis.find(sushi => sushi.id === id)
+    this.setState({
+      sushisEaten: [...this.state.sushisEaten, sushi],
+      money: (this.state.money - sushi.price)
+    })
+  }
+  updateWallet = event => {
+    event.preventDefault()
+    this.setState({
+      money: +(this.state.money) + +(event.target.money.value)
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer 
+          money={this.state.money} 
+          eatSushi={this.eatSushi} 
+          sushis={this.state.sushis} 
+        />
+        <Table 
+          updateWallet={this.updateWallet} 
+          money={this.state.money} 
+          sushis={this.state.sushisEaten}
+          />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,47 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+class SushiContainer extends React.Component{
+
+  state={
+    page: 25
+  }
+
+  sushiToRender = (page) => {
+       //Pseudo Pagination -- would rather do this in the fetch but forgot how to do query params//
+    return this.props.sushis.filter(sushi=>{
+      return parseInt((+(sushi.id) - 1)/4) + 1 === page
+    })
+  }
+
+  updatePage = () => {
+    if(this.sushiToRender(this.state.page + 1).length){
+    this.setState({
+      page: this.state.page + 1
+    })}else{
+      this.setState({
+        page: 1
+      })
+    }
+  }
+
+ renderSushi = () =>{
+   return this.sushiToRender(this.state.page).map(sushi=> <Sushi money={this.props.money} eatSushi={this.props.eatSushi} key={sushi.id} {...sushi}/>)
+
+  }
+  render(){
+    return (
+      <Fragment>
+        <div className="belt">
+          {
+            this.renderSushi()
+          }
+          <MoreButton updatePage={this.updatePage}/>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default SushiContainer
